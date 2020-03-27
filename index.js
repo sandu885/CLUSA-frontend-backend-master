8525fb23fbbb8f46cf33efc81ff25a513fd65186
Josh Nicholas <joshcooldev@gmail.com>
2020-03-27 18:40:36 +0530

    changed db uri

--- index.js
+++ index.js
@@ -24,8 +24,6 @@ if (app.get('env') === 'development') {
   console.log("In development env...");
 }
 
-databaseUri = "mongodb://localhost:27017/intern_grand_db";
-
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
