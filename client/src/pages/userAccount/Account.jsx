34a79cba5ebc61ea25a3eeff41fbe5b9d454b5b4
Josh Nicholas <joshcooldev@gmail.com>
2020-04-06 22:05:24 +0530

    button disable in instruction page

--- client/src/pages/userAccount/Account.jsx
+++ client/src/pages/userAccount/Account.jsx
@@ -193,11 +193,14 @@ class Account extends Component {
   }
 
   showApplicationWithInfo = () => {
+
     let statusToCheck = ["applying", "applied&ProgramOnGoing", "inReview"];
-    if(!statusToCheck.includes(this.state.status)) {
-      return alert("Not allow to edit application since your application is already approved");
-    }
-    this.props.history.push('/internship-information');
+    let currentStatus = (statusToCheck.includes(this.state.status)) ? "" : "1";
+
+    //if(currentStatus == "1") {
+      //return alert("Not allow to edit application since your application is already approved");
+    //}
+    this.props.history.push(`/internship-information?status=${currentStatus}`);
   }
 
   clickApplyBtn = () => {
